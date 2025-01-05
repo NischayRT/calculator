70937ff05006487009a81a855202affede33ce82
Nischay Reddy <105917589+NischayRT@users.noreply.github.com>
2025-01-05 15:47:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,22 @@ function add(x) {
 	inp.value += x;
 	console.log("Enter add\n");
 }
-
+function fact() {
+	console.log("factorial!\n");
+	if (inp.value == 0) {
+		return 1;
+	} else if (inp.value > 0) {
+		inp.value = factorial(inp.value);
+		return inp.value;
+	} else {
+		inp.value = "negative value cannot be factorized";
+		return inp.value;
+	}
+}
+function factorial(x) {
+	if (x == 0 || x == 1) return 1;
+	return x * factorial(x - 1);
+}
 function del() {
 	inp.value = inp.value.substr(0, inp.value.length - 1);
 	console.log("delete value\n");
